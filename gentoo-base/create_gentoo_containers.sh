e6a90cadaeab3a7066116807059a02667cde1648
Rahil Bhimjiani <rahil3108@gmail.com>
2023-05-12 13:03:00 +0530

    misc

--- gentoo-base/create_gentoo_containers.sh
+++ gentoo-base/create_gentoo_containers.sh
@@ -7,7 +7,6 @@ time { cd ${HOME}/containers/gentoo/gentoo-base && \
 	   -f ${HOME}/containers/gentoo/gentoo-base/Containerfile \
 	   -v ${HOME}/.local/share/containers/storage/volumes/gentoo_repo/_data:/var/db/repos \
 	   -v ${HOME}/.local/share/containers/storage/volumes/gentoo_distfiles/_data:/var/cache/distfiles \
-	   -v ${HOME}/.local/share/containers/storage/volumes/gentoo_binpkgs/_data:/var/cache/binpkgs \
 	   --secret=id=ansible-homelab-vaultpass,src=${HOME}/containers/gentoo/gentoo-base/vaultpass \
 	   --build-arg=LOCAL_MIRROR="http://alienware.hl.rahil.website/" \
 	   --build-arg=ANSIBLE_REPO="https://gitlab.com/rahilarious/ansible-homelab.git" \
