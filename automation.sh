2155ca02cd55daf0841efe97a7ee4fe62790ac04
Rahil Bhimjiani <rahil3108@gmail.com>
2023-08-01 21:45:33 +0530

    automation script - first try

--- automation.sh
+++ automation.sh
@@ -1,29 +1,65 @@
 #!/usr/bin/bash
 
+# exit on errors
 set -e
 
 ### variables
 BUILD_TAG=$(date +%Y%m%d%H%M%S)
-read -p "Enter Github API token: " GITHUB_API_TOKEN
-read -p "Enter Docker API token: " DOCKER_API_TOKEN
-MICROARCHITECTURES="x86-64-v2 x86-64-v3"
-GIT_REPO_PATH="${HOME}/containers/gentoo"
+REGISTRIES_WITH_USERNAME="ghcr.io/rahilarious docker.io/rahilarious"
+MAIN_REGISTRY_WITH_USERNAME="ghcr.io/rahilarious"
+MICROARCHS="x86-64-v2 x86-64-v3"
+CURRENT_DIR="$(realpath $(dirname $0))"
+PKG_DIRS="$(ls -d ${CURRENT_DIR}/*/)"
 
-cd ${GIT_REPO_PATH}
-for microarch in ${MICROARCHITECTURES}
+### CODE
+cd ${CURRENT_DIR}
+for MICROARCH in ${MICROARCHS}
 do
-    git switch ${microarch}
-    for PKG_DIR in $(ls -d ${GIT_REPO_PATH}/*/)
+    git switch ${MICROARCH}
+    for PKG_DIR in ${PKG_DIRS}
     do
-	PKG=$(basename ${PKG_DIR})
-	if [[ -f ${PKG_DIR}extra_tag ]]; then
-	    read EXTRA_TAG < ${PKG_DIR}extra_tag
-    	    echo "Extra tag: ${EXTRA_TAG}"
-	fi
+	PKG_NAME=$(basename ${PKG_DIR} | cut -d- -f1 --complement)
 
-	echo $PKG_DIR
-	echo "Package name: ${PKG}"
+	## BUILD IMAGE
+	echo "##########       ${PKG_NAME} (${MICROARCH})         ############"
+	if [[ -s ${PKG_DIR}create_gentoo_containers.sh ]]
+	then
+	    ${PKG_DIR}create_gentoo_containers.sh
+	else
+	    echo "ERROR: can't find build script. Exiting..."
+	    exit 1
+	fi
+	
+
+	### TAG & PUSH IMAGE
+	for REGISTRY_WITH_USERNAME in ${REGISTRIES_WITH_USERNAME}
+	do
+	    ### TAG & PUSH BASE TAG
+	    if [[ ${REGISTRY_WITH_USERNAME} != ${MAIN_REGISTRY_WITH_USERNAME} ]]
+	    then
+		sudo podman tag ${MAIN_REGISTRY_WITH_USERNAME}/${PKG_NAME}:${MICROARCH} ${REGISTRY_WITH_USERNAME}/${PKG_NAME}:${MICROARCH}
+	    fi
+	    sudo podman push ${REGISTRY_WITH_USERNAME}/${PKG_NAME}:${MICROARCH}
+	    
+	    ### TAG & PUSH EXTRA TAG(s)
+	    if [[ -s ${PKG_DIR}extra_tag ]]
+	    then
+		read EXTRA_TAG < ${PKG_DIR}extra_tag
+		sudo podman tag ${MAIN_REGISTRY_WITH_USERNAME}/${PKG_NAME}:${MICROARCH} ${REGISTRY_WITH_USERNAME}/${PKG_NAME}:${MICROARCH}-${EXTRA_TAG}
+		sudo podman tag ${MAIN_REGISTRY_WITH_USERNAME}/${PKG_NAME}:${MICROARCH} ${REGISTRY_WITH_USERNAME}/${PKG_NAME}:${MICROARCH}-${EXTRA_TAG}-${BUILD_TAG}
+
+		sudo podman push ${REGISTRY_WITH_USERNAME}/${PKG_NAME}:${MICROARCH}-${EXTRA_TAG}
+		sudo podman push ${REGISTRY_WITH_USERNAME}/${PKG_NAME}:${MICROARCH}-${EXTRA_TAG}-${BUILD_TAG}
+	    else
+		sudo podman tag ${MAIN_REGISTRY_WITH_USERNAME}/${PKG_NAME}:${MICROARCH} ${REGISTRY_WITH_USERNAME}/${PKG_NAME}:${MICROARCH}-${BUILD_TAG}
+
+		sudo podman push ${REGISTRY_WITH_USERNAME}/${PKG_NAME}:${MICROARCH}-${BUILD_TAG}
+	    fi
+	done
     done
 done
 
 git switch main
+
+echo "Congratulations !!! You can go for a break now."
+
